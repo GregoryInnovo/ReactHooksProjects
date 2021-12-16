6e1a579c3e49e3d50a564722a5610f19be38cb4d
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-12-16 22:44:36 +0000

    create and add new tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { TaskRow } from "./components/TaskRow";
 import { TaskBanner } from "./components/TaskBanner";
+import { TaskCreator } from "./components/TaskCreator";
 import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -18,6 +19,12 @@ export default function App() {
     { name: "Task Three", done: true },
     { name: "Task Four", done: false },
   ]);
+
+  const createNewTask = taskName => {
+    if(!taskItems.find(t => t.name === taskName)) {
+      setTaskItems([...taskItems, {name: taskName, done: false}])
+    }
+  }
 
   const toggleTask = (task) =>
     setTaskItems(
@@ -42,6 +49,7 @@ export default function App() {
   return (
     <>
       <TaskBanner userName={userName} taskItems={taskItems} />
+      <TaskCreator callback={createNewTask} />
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 700 }} aria-label="customized table">
           <TableHead>
---
+++ src/components/TaskCreator.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+export const TaskCreator = (props) => {
+  const [newTaskName, setNewTaskName] = useState("");
+
+  const updateTaskValue = (e) => setNewTaskName(e.target.value);
+
+  const createNewTask = () => {
+    props.callback(newTaskName)
+    console.log(newTaskName);
+    setNewTaskName("");
+  };
+
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        alignItems: "space-between",
+        width: "90%",
+        m: "auto",
+        mb: 1,
+        mt: 1,
+      }}
+    >
+      <TextField
+        fullWidth
+        label="Add task"
+        id="fullWidth"
+        value={newTaskName}
+        onChange={updateTaskValue}
+      />
+      <Button
+        sx={{
+          ml: 1,
+        }}
+        variant="contained"
+        onClick={createNewTask}
+      >
+        Add
+      </Button>
+    </Container>
+  );
+};
